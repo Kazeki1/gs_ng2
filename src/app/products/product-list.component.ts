4004e068fcd88a0c10d5630e0b065ea55ad8080d
kazeki1 <wmj.floyd@gmail.com>
2017-08-19 22:52:51 -0600

    wired in filter

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,7 +11,16 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 100;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cheese';
+
+  _listFilter: string
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+  filteredProducts: IProduct[];
   products: IProduct[]= [
     {
       'productId': 1,
@@ -45,6 +54,18 @@ export class ProductListComponent implements OnInit {
 
     }
   ];
+
+  constructor() {
+    this.filteredProducts = this.products;
+    this.listFilter = 'cheese';
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
