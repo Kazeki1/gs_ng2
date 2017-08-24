3138cd5c1e0d99e0d28b3218864beb6c3d5eab99
kazeki1 <wmj.floyd@gmail.com>
2017-08-24 15:48:28 -0600

    added backbutton product-detail.component.ts/html

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   // selector: 'pm-product-detail',
@@ -10,9 +11,30 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
-
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) {
+              console.log('ID: ' + this._route.snapshot.paramMap.get('id'));
+              }
   ngOnInit() {
+    const id = +this._route.snapshot.paramMap.get('id'); /*using const instead of let*/
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Buffalo Blue Cheese',
+      'productQuantity': '20',
+      'productCode': 'GDN-0021',
+      'releaseDate': 'March 11, 2017',
+      'receiveDate': '170421',
+      'expDate': '171211',
+      'onShelfDate': '170501',
+      'doSign': 'Wm',
+      'description': '8 oz glass bottle',
+      'price': 6.99,
+      'starRating': 3.2,
+      'imageUrl': '../assets/images/Kaz_DipMix/BuffaloBlueCheese.jpg'
+    };
   }
-
+    onBack(): void {
+      this._router.navigate(['/products']);
+    }
 }
