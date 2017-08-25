42cd6b82bf68ac4d07e4d79865070646610c7f26
kazeki1 <wmj.floyd@gmail.com>
2017-08-25 00:22:20 -0600

    added SharedModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductService } from './products/product.service';
+// import { ProductListComponent } from './products/product-list.component';
+// import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+// import { StarComponent } from './shared/star.component';
+// import { ProductService } from './products/product.service';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductDetailComponent } from './products/product-detail.component';
+// import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductGuardService } from './products/product-guard.service';
+// import { ProductGuardService } from './products/product-guard.service';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
-    { path: 'products', component: ProductListComponent },
-    { path: 'products/:id',
-      canActivate: [ ProductGuardService ],
-      component: ProductDetailComponent },
+  imports: [ BrowserModule, HttpClientModule, RouterModule.forRoot([
     { path: 'welcome', component: WelcomeComponent },
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-  ]) ],
-  declarations: [ AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent, WelcomeComponent ],
-  bootstrap: [ AppComponent, ProductListComponent ],
-  providers: [ ProductService, ProductGuardService ]
-
+  ]), ProductModule ],
+  declarations: [ AppComponent, WelcomeComponent ],
+  bootstrap: [ AppComponent]
 })
 export class AppModule { }
